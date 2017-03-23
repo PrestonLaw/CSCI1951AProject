1505b1646ccf7f76c80a658c676b892bfde0481f
natalieroe1 <natalie_roe@brown.edu>
2017-03-22 22:25:57 -0400

    output most common words data to csv and other fixes

--- features.py
+++ features.py
@@ -5,6 +5,7 @@ from os.path import dirname, realpath
 
 from mapreduce import mapreduce
 import collections
+import csv
 
 
 # record: (Id,Title,Description,LikeCount,DislikeCount,ViewCount,FavoriteCount,CommentCount)
@@ -24,24 +25,30 @@ def main():
     with open('videoStats.csv', 'rb') as f:
         data = [line.split(',') for line in f]
 
-    sc = mapreduce()
-    result = sc.parallelize(data[1:], 128) \
-                        .map(mapper1) \
-                        .reduceByKey(reducer) \
-                        .sortByKey(True) \
-                        .collect()
+        sc = mapreduce()
+        result = sc.parallelize(data[1:], 128) \
+                            .map(mapper1) \
+                            .reduceByKey(reducer) \
+                            .sortByKey(True) \
+                            .collect()
 
-    sc.stop()
+        sc.stop()
 
-    topVids = result[len(result)-51:]
-    l = []
+        topVids = result[len(result)-51:]
+        l = []
 
-    for vid in topVids:
-        l.extend(vid[1][0].lower().split(' '))
-        l.extend(vid[1][1].lower().split(' '))
+        for vid in topVids:
+            l.extend(vid[1][0].lower().split(' '))
+            l.extend(vid[1][1].lower().split(' '))
 
-    counter = collections.Counter(l)
-    print counter.most_common()
+        counter = collections.Counter(l)
+
+        with open('data.csv', 'wb') as c:
+            writer = csv.writer(c)
+            writer.writerow(['Word', 'Count'])
+
+            for key,count in counter.most_common():
+                writer.writerow([key, count])
 
 if __name__ == '__main__':
     main()
